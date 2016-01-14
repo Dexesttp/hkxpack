0e331458c6674c0e80d01974cd6b13944b089856
DexesTTP <dexes.ttp@gmail.com>
2016-01-14 23:38:56 +0100

    Reove deprecated classes

--- src/com/dexesttp/hkxpack/resources/InvalidFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.dexesttp.hkxpack.resources;
-
-public class InvalidFormatException extends Exception {
-	private static final long serialVersionUID = -2824271887951719499L;
-	public InvalidFormatException(int version) {
-		super("Invalid version : " + version);
-	}
-}
