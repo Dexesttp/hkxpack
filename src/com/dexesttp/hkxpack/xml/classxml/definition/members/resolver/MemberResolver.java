4078f22063e4d17316cb336873e37aea9d15ddac
DexesTTP <dexes.ttp@gmail.com>
2016-01-15 04:42:17 +0100

    Prettier code for Enum value checking.

--- src/com/dexesttp/hkxpack/xml/classxml/definition/members/resolver/MemberResolver.java
+++ src/com/dexesttp/hkxpack/xml/classxml/definition/members/resolver/MemberResolver.java
@@ -2,53 +2,40 @@ package com.dexesttp.hkxpack.xml.classxml.definition.members.resolver;
 
 import com.dexesttp.hkxpack.xml.classxml.definition.members.ResolvedMember;
 
-// TODO fix this ugly mess.
 public class MemberResolver {
+	
+	// TODO fix this ugly mess.
 	public static ResolvedMember resolve(String name, String vtype, String ctype, String vsubtype) {
-		DirectMemberResolver resolver1 = getDirectMemberResolver(vtype);
-		ArrayMemberResolver resolver2 = getArrayMemberResolver(vtype);
-		PtrMemberResolver resolver3 = getPtrMemberResolver(vtype);
+		DirectMemberResolver resolver1 = getEnumVal(DirectMemberResolver.class, vtype);
+		ArrayMemberResolver resolver2 = getEnumVal(ArrayMemberResolver.class, vtype);
+		PtrMemberResolver resolver3 = getEnumVal(PtrMemberResolver.class, vtype);
 		if(resolver1 != null) {
 			return resolver1.resolve(name);
 		} else if(resolver2 != null){
-			resolver1 = getDirectMemberResolver(vsubtype);
-			resolver3 = getPtrMemberResolver(vsubtype);
+			resolver1 = getEnumVal(DirectMemberResolver.class, vsubtype);
+			resolver3 = getEnumVal(PtrMemberResolver.class, vsubtype);
 			if(resolver1 != null) {
 				return resolver2.resolve(resolver1.resolve(name), name);
 			} else if(resolver3 != null) {
-				resolver1 = getDirectMemberResolver(ctype);
+				resolver1 = getEnumVal(DirectMemberResolver.class, ctype);
 				if(resolver1 != null)
 					return resolver2.resolve(resolver3.resolve(resolver1.resolve(name),  name), name);
 			}
 		} else if(resolver3 != null) {
-			resolver1 = getDirectMemberResolver(ctype);
+			resolver1 = getEnumVal(DirectMemberResolver.class, ctype);
 			if(resolver1 != null)
 				return resolver3.resolve(resolver1.resolve(name),  name);
 		}
 		throw new IllegalArgumentException(name + " : " + vtype + "//" + ctype + "//" + vsubtype);
 	}
 	
-	public static DirectMemberResolver getDirectMemberResolver(String type) {
+	/*
+	 * Thanks to user2910265 for finally solving this thing
+	 * http://stackoverflow.com/a/21214662
+	 */
+	private static <E extends Enum<E>> E getEnumVal(Class<E> _enumClass, String name) {
 		try {
-			return DirectMemberResolver.valueOf(type);
-		}
-		catch(IllegalArgumentException e) {
-			return null;
-		}
-	}
-	
-	public static ArrayMemberResolver getArrayMemberResolver(String type) {
-		try {
-			return ArrayMemberResolver.valueOf(type);
-		}
-		catch(IllegalArgumentException e) {
-			return null;
-		}
-	}
-	
-	public static PtrMemberResolver getPtrMemberResolver(String type) {
-		try {
-			return PtrMemberResolver.valueOf(type);
+			return Enum.valueOf(_enumClass, name);
 		}
 		catch(IllegalArgumentException e) {
 			return null;
