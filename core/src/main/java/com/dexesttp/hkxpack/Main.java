755235b2b29990d862f725d2b40712380451efb4
DexesTTP <dexes.ttp@gmail.com>
2016-01-21 22:19:28 +0100

    Updated main for testing purposes.

--- core/src/main/java/com/dexesttp/hkxpack/Main.java
+++ core/src/main/java/com/dexesttp/hkxpack/Main.java
@@ -1,12 +1,19 @@
 package com.dexesttp.hkxpack;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
+
+import com.dexesttp.hkxpack.hkx.header.HeaderData;
+import com.dexesttp.hkxpack.hkx.header.HeaderInterface;
+import com.dexesttp.hkxpack.hkx.header.SectionData;
+import com.dexesttp.hkxpack.hkx.header.SectionInterface;
 
 public class Main {
 	/**
@@ -15,6 +22,25 @@ public class Main {
 	 */
 	public void exec(String fileName, String outputFile) {
 		try {
+			// Read file
+			File file = new File(fileName);
+			// Read header
+			HeaderInterface headInt = new HeaderInterface();
+			headInt.connect(file);
+			HeaderData header = headInt.extract();
+			headInt.close();
+			
+			// Read header sections.
+			SectionInterface sectInt = new SectionInterface();
+			sectInt.connect(file, header);
+			// See documentation to explain why this.
+			SectionData classnamesHead = sectInt.extract(0);
+			SectionData dataHead = sectInt.extract(2);
+			
+			System.out.println(classnamesHead.name);
+			//Read classnames
+			
+			
 			// Output result
 	        TransformerFactory transformerFactory =
 	                 TransformerFactory.newInstance();
@@ -27,7 +53,13 @@ public class Main {
 				new StreamResult(System.out);
 			else
 				new StreamResult(new File(outputFile));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (TransformerException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
