627c19bc535440820790e057fd2b27aaee975a08
DexesTTP <dexes.ttp@gmail.com>
2016-01-22 14:03:40 +0100

    Removed SerializedMemberResolver, not necessary anymore.

--- core/src/main/java/com/dexesttp/hkxpack/xml/classxml/definition/members/resolver/SerializedMemberResolver.java
+++
@@ -1,6 +0,0 @@
-package com.dexesttp.hkxpack.xml.classxml.definition.members.resolver;
-
-public class SerializedMemberResolver implements BaseMemberResolver {
-	public void resolve(String name, String classname) {
-	}
-}
