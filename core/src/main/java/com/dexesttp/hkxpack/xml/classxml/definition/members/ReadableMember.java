2d35ded02b3aae7ccf372418d21a0161cf633bd9
DexesTTP <dexes.ttp@gmail.com>
2016-01-22 14:38:19 +0100

    Defined a way to resolve a classXML member into a struct member.

--- core/src/main/java/com/dexesttp/hkxpack/xml/classxml/definition/members/ReadableMember.java
+++ core/src/main/java/com/dexesttp/hkxpack/xml/classxml/definition/members/ReadableMember.java
@@ -1,17 +1,22 @@
 package com.dexesttp.hkxpack.xml.classxml.definition.members;
 
+import com.dexesttp.hkxpack.hkx.structs.Member;
 import com.dexesttp.hkxpack.xml.classxml.definition.members.resolver.BaseMemberResolver;
 
 public class ReadableMember extends ClassXMLMember {
 	private final BaseMemberResolver resolver;
-
-	public ReadableMember(String name, String classname, BaseMemberResolver resolver) {
+	private final long offset;
+	private final String flag;
+	
+	@SuppressWarnings("rawtypes") 
+	public ReadableMember(String name, String classname, String flags, long offset, Enum enumInst) {
 		super(name, classname);
-		this.resolver = resolver;
+		this.offset = offset;
+		this.flag = flags;
+		resolver = (BaseMemberResolver) enumInst;
 	}
 
-	public ReadableMember(String name, String classname, String flags, String offset, Enum enumInst) {
-		super(name, classname);
-		resolver = (BaseMemberResolver) enumInst;
+	public Member getStruct() {
+		return new Member(resolver.getSize(), offset);
 	}
 }
