cdf718d74caf82cd8d3a623bcb151341b0367d8f
DexesTTP <dexes.ttp@gmail.com>
2016-01-22 10:56:53 +0100

    Added dummy methods to ReadableClass

--- core/src/main/java/com/dexesttp/hkxpack/xml/classxml/definition/classes/ReadableClass.java
+++ core/src/main/java/com/dexesttp/hkxpack/xml/classxml/definition/classes/ReadableClass.java
@@ -1,5 +1,12 @@
 package com.dexesttp.hkxpack.xml.classxml.definition.classes;
 
+import org.w3c.dom.Node;
+
+import com.dexesttp.hkxpack.hkx.data.Data1Interface;
+import com.dexesttp.hkxpack.hkx.data.Data2Interface;
+import com.dexesttp.hkxpack.hkx.structs.DataInterface;
+import com.dexesttp.hkxpack.hkx.structs.Struct;
+
 public class ReadableClass extends ClassXML {
 	protected final String classname;
 	protected final int classID;
@@ -17,5 +24,15 @@ public class ReadableClass extends ClassXML {
 	@Override
 	public int getClassID() {
 		return classID;
+	}
+
+	public Struct getStruct() {
+		// TODO this thing right here.
+		return null;
+	}
+
+	public Node resolve(Struct currentStruct, DataInterface data, Data1Interface data1, Data2Interface data2) {
+		// TODO this other thing right there.
+		return null;
 	}
 }
