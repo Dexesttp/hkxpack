0e16efbdfd28c11b637e7871412d7364a07de802
DexesTTP <dexes.ttp@gmail.com>
2016-02-20 19:44:28 +0100

    Removed unused exceptions.

--- core/src/main/java/com/dexesttp/hkxpack/resources/exceptions/InvalidXMLException.java
+++
@@ -1,9 +0,0 @@
-package com.dexesttp.hkxpack.resources.exceptions;
-
-public class InvalidXMLException extends Exception {
-	private static final long serialVersionUID = -3569435660233268382L;
-
-	public InvalidXMLException(String message) {
-		super(message);
-	}
-}
