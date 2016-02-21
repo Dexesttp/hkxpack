1912f07d31415c04f83fa8c729e609f18588af41
DexesTTP <dexes.ttp@gmail.com>
2016-02-21 12:54:49 +0100

    Removed use of Main in TestView

--- cli/src/main/java/com/dexesttp/hkxpack/cli/TestView.java
+++ cli/src/main/java/com/dexesttp/hkxpack/cli/TestView.java
@@ -1,37 +1,103 @@
 package com.dexesttp.hkxpack.cli;
 
-import com.dexesttp.hkxpack.Main;
+import java.io.File;
+
 import com.dexesttp.hkxpack.cli.utils.RandomUtils;
+import com.dexesttp.hkxpack.data.HKXFile;
+import com.dexesttp.hkxpack.descriptor.HKXDescriptorFactory;
+import com.dexesttp.hkxpack.descriptor.HKXEnumResolver;
+import com.dexesttp.hkxpack.hkxreader.HKXReader;
 import com.dexesttp.hkxpack.resources.DisplayProperties;
+import com.dexesttp.hkxpack.resources.LoggerUtil;
+import com.dexesttp.hkxpack.tagreader.TagXMLReader;
+import com.dexesttp.hkxpack.tagwriter.TagXMLWriter;
 
 public class TestView {
 	public static void main(String[] args) {
-		read(args);
+		xmlTest(args);
 	}
 	
 	public static void read(String[] args) {
-		Main main = new Main();
-		String fileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\DeathChest01.hkx";
-		String outName =  RandomUtils.makeFromFileName(fileName);
+		String inputFileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\DeathChest01.hkx";
+		String outputFileName =  RandomUtils.makeFromFileName(inputFileName);
 		DisplayProperties.displayDebugInfo = true;
 		DisplayProperties.displayFileDebugInfo = true;
 		DisplayProperties.displayReadTypesInfo = true;
 		DisplayProperties.displayClassImportsInfo = true;
 		DisplayProperties.displayEmbeddedData = true;
-		main.read(fileName, outName);
+		try {
+			// Read file
+			File inFile = new File(inputFileName);
+			HKXEnumResolver enumResolver = new HKXEnumResolver();
+			HKXDescriptorFactory descriptorFactory;
+				descriptorFactory = new HKXDescriptorFactory(enumResolver);
+			HKXReader reader = new HKXReader(inFile, descriptorFactory, enumResolver);
+			HKXFile hkxFile = reader.read();
+			
+			// Write file
+			File outFile = new File(outputFileName);
+			TagXMLWriter writer = new TagXMLWriter(outFile);
+			writer.write(hkxFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void write(String[] args) {
-		Main main = new Main();
-		String fileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\Idle.xml";
-		String outName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\Idle-out.hkx";
-		//String outName = "";
+		String inputFileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\Idle.xml";
+		String outputFileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\Idle-out.hkx";
 		DisplayProperties.displayDebugInfo = true;
 		DisplayProperties.displayFileDebugInfo = true;
 		DisplayProperties.displayReadTypesInfo = true;
 		DisplayProperties.displayClassImportsInfo = true;
 		DisplayProperties.displayEmbeddedData = true;
+
+		try {
+			// Read file
+			File inFile = new File(inputFileName);
+			HKXEnumResolver enumResolver = new HKXEnumResolver();
+			HKXDescriptorFactory descriptorFactory;
+				descriptorFactory = new HKXDescriptorFactory(enumResolver);
+			TagXMLReader reader = new TagXMLReader(inFile, descriptorFactory, enumResolver);
+			reader.read();
+			
+			// TODO handle file write
+			System.out.println(outputFileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		main.write(fileName, outName);
+		// Print logs
+        LoggerUtil.output();
+	}
+	
+	public static void xmlTest(String[] args) {
+		String inputFileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\DeathChest01.xml";
+		String outputFileName = "D:\\Documents\\SANDBOX\\FO4\\hkx_files\\DeathChest01-out.xml";
+		DisplayProperties.displayDebugInfo = true;
+		DisplayProperties.displayFileDebugInfo = true;
+		DisplayProperties.displayReadTypesInfo = true;
+		DisplayProperties.displayClassImportsInfo = true;
+		DisplayProperties.displayEmbeddedData = true;
+
+		try {
+			// Read file
+			File inFile = new File(inputFileName);
+			HKXEnumResolver enumResolver = new HKXEnumResolver();
+			HKXDescriptorFactory descriptorFactory;
+				descriptorFactory = new HKXDescriptorFactory(enumResolver);
+			TagXMLReader reader = new TagXMLReader(inFile, descriptorFactory, enumResolver);
+			HKXFile hkxFile = reader.read();
+			
+			// Write file
+			File outFile = new File(outputFileName);
+			TagXMLWriter writer = new TagXMLWriter(outFile);
+			writer.write(hkxFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Print logs
+        LoggerUtil.output();
 	}
 }
