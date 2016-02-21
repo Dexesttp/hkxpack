886c5ab4a4beeaba3521c96e165ec037059e908b
DexesTTP <dexes.ttp@gmail.com>
2016-02-21 12:54:32 +0100

    Added TagXMLReader template.

---
+++ core/src/main/java/com/dexesttp/hkxpack/tagreader/TagXMLReader.java
@@ -0,0 +1,20 @@
+package com.dexesttp.hkxpack.tagreader;
+
+import java.io.File;
+
+import com.dexesttp.hkxpack.data.HKXFile;
+import com.dexesttp.hkxpack.descriptor.HKXDescriptorFactory;
+import com.dexesttp.hkxpack.descriptor.HKXEnumResolver;
+
+public class TagXMLReader {
+
+	public TagXMLReader(File inFile, HKXDescriptorFactory descriptorFactory, HKXEnumResolver enumResolver) {
+		// TODO TagXMLReader initialization.
+	}
+
+	public HKXFile read() {
+		// TODO TagXML file reading.
+		return null;
+	}
+
+}
