a1b058096f95e3c6c9c9a1015c34cecb8e9fad24
DexesTTP <dexes.ttp@gmail.com>
2016-02-28 21:24:59 +0100

    Fix : wrong output for QSTransform objects

--- core/src/main/java/com/dexesttp/hkxpack/tagwriter/TagXMLDirectMemberHandler.java
+++ core/src/main/java/com/dexesttp/hkxpack/tagwriter/TagXMLDirectMemberHandler.java
@@ -14,11 +14,17 @@ class TagXMLDirectMemberHandler {
 	String getStringValue(HKXDirectMember<?> member) {
 		if(member.get() instanceof Double[]) {
 			Double[] contents = (Double[]) member.get();
-			String contentsAccu = "(";
-			for(int i = 0; i < contents.length; i++) {
-				contentsAccu += "" + contents[i] + " ";
+			if(contents.length <= 4) {
+				String contentsAccu = "(";
+				for(int i = 0; i < contents.length; i++) {
+					contentsAccu += "" + contents[i] + " ";
+				}
+				return contentsAccu.substring(0, contentsAccu.length() - 1) + ")";
+			} else {
+				return "(" + contents[0] + " " + contents[1] + " " + contents[2] + " " + ")" +
+						"(" + contents[3] + " " + contents[4] + " " + contents[5]+ " " + contents[6] + ")" +
+						"(" + contents[7] + " " + contents[8] + " " + contents[9] + " " + ")";
 			}
-			return contentsAccu.substring(0, contentsAccu.length() - 1) + ")";
 		}
 		if(member.get() instanceof Character)
 			return "" + (int) ((char) member.get());
