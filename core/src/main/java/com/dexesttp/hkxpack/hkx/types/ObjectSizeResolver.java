34266eeb77306db07ab5a7a4ce376e7f53997ff8
DexesTTP <dexes.ttp@gmail.com>
2016-03-06 15:02:38 +0100

    Fixed padding calculus while using enums.

--- core/src/main/java/com/dexesttp/hkxpack/hkx/types/ObjectSizeResolver.java
+++ core/src/main/java/com/dexesttp/hkxpack/hkx/types/ObjectSizeResolver.java
@@ -6,6 +6,7 @@ import com.dexesttp.hkxpack.data.HKXObject;
 import com.dexesttp.hkxpack.descriptor.HKXDescriptor;
 import com.dexesttp.hkxpack.descriptor.HKXDescriptorFactory;
 import com.dexesttp.hkxpack.descriptor.enums.HKXType;
+import com.dexesttp.hkxpack.descriptor.enums.HKXTypeFamily;
 import com.dexesttp.hkxpack.descriptor.exceptions.ClassFileReadError;
 import com.dexesttp.hkxpack.descriptor.members.HKXMemberTemplate;
 import com.dexesttp.hkxpack.hkx.HKXUtils;
@@ -21,9 +22,6 @@ public class ObjectSizeResolver {
 		case TYPE_NONE:
 		case TYPE_VOID:
 			return 0X00;
-		case TYPE_ENUM:
-		case TYPE_FLAGS:
-			return 0X04;
 	// Base values
 		case TYPE_BOOL:
 			return 0X01;
@@ -85,6 +83,8 @@ public class ObjectSizeResolver {
 			return 0;
 		long bestSnap = getSnap(descriptor, descriptorFactory);
 		HKXMemberTemplate lastTemplate = templates.get(templates.size() - 1);
+		if(lastTemplate.vtype.getFamily() == HKXTypeFamily.ENUM)
+			return HKXUtils.snapSize(lastTemplate.offset + MemberSizeResolver.getSize(lastTemplate.vsubtype), bestSnap);
 		if(lastTemplate.vtype != HKXType.TYPE_STRUCT)
 			return HKXUtils.snapSize(lastTemplate.offset + MemberSizeResolver.getSize(lastTemplate.vtype), bestSnap);
 		HKXDescriptor internalDescriptor = descriptorFactory.get(lastTemplate.target);
@@ -97,7 +97,9 @@ public class ObjectSizeResolver {
 		for(int i = 0; i < list.size(); i++) {
 			HKXMemberTemplate template = list.get(i);
 			long currSnap = 0;
-			if(template.vtype != HKXType.TYPE_STRUCT)
+			if(template.vtype.getFamily() == HKXTypeFamily.ENUM)
+				currSnap = primitiveSnap(template.vsubtype);
+			else if(template.vtype != HKXType.TYPE_STRUCT)
 				currSnap = primitiveSnap(template.vtype);
 			else {
 				HKXDescriptor internalDescriptor = descriptorFactory.get(template.target);
@@ -119,6 +121,8 @@ public class ObjectSizeResolver {
 			return 0;
 		long bestSnap = getSnap(object);
 		HKXMemberTemplate lastTemplate = templates.get(templates.size() - 1);
+		if(lastTemplate.vtype.getFamily() == HKXTypeFamily.ENUM)
+			return HKXUtils.snapSize(lastTemplate.offset + MemberSizeResolver.getSize(lastTemplate.vsubtype), bestSnap);
 		if(lastTemplate.vtype != HKXType.TYPE_STRUCT)
 			return HKXUtils.snapSize(lastTemplate.offset + MemberSizeResolver.getSize(lastTemplate.vtype), bestSnap);
 		HKXObject internalObject = (HKXObject) object.members().get(templates.size() - 1);
@@ -131,7 +135,9 @@ public class ObjectSizeResolver {
 		for(int i = 0; i < list.size(); i++) {
 			HKXMemberTemplate template = list.get(i);
 			long currSnap = 0;
-			if(template.vtype != HKXType.TYPE_STRUCT)
+			if(template.vtype.getFamily() == HKXTypeFamily.ENUM)
+				currSnap = primitiveSnap(template.vsubtype);
+			else if(template.vtype != HKXType.TYPE_STRUCT)
 				currSnap = primitiveSnap(template.vtype);
 			else {
 				HKXObject internalObject = (HKXObject) object.members().get(i);
