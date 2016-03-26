22ae2b4546766d88a7e56983b2f616653e9c8e54
DexesTTP <dexes.ttp@gmail.com>
2016-03-26 14:13:48 +0100

    Added unit tests for Signed byte analysis and Unsigned byte analysis

---
+++ core/src/test/java/com/dexesttp/hkxpack/resources/byteutils/SLongByteUtilsTest.java
@@ -0,0 +1,42 @@
+package com.dexesttp.hkxpack.resources.byteutils;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class SLongByteUtilsTest {
+	@Test
+	public void getSLongSize2WorksFor0() {
+		assertArrayEquals("SLong(2) : 0 => 0",
+				new byte[]{0, 0},
+				SLongByteUtils.fromLong(0, 2));
+	}
+
+	@Test
+	public void getSLongSize2WorksFor1() {
+		assertArrayEquals("SLong(2) : 1 => 1",
+				new byte[]{1, 0},
+				SLongByteUtils.fromLong(1, 2));
+	}
+
+	@Test
+	public void getSLongSize2WorksForMinus1() {
+		assertArrayEquals("SLong(2) : -1 => -1",
+				new byte[]{-1, -1},
+				SLongByteUtils.fromLong(-1, 2));
+	}
+
+	@Test
+	public void getSILongSize2WorksForMaxValue() {
+		assertArrayEquals("SLong(2) : 32767 => 32767",
+				new byte[]{-1, 127},
+				SLongByteUtils.fromLong(32767, 2));
+	}
+
+	@Test
+	public void getSIntSize2WorksForMinValue() {
+		assertArrayEquals("SLong(2) : -32768 => -32768",
+				new byte[]{0, -128},
+				SLongByteUtils.fromLong(-32768, 2));
+	}
+}
---
+++ core/src/test/java/com/dexesttp/hkxpack/resources/byteutils/ULongByteUtilsTest.java
@@ -0,0 +1,35 @@
+package com.dexesttp.hkxpack.resources.byteutils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ULongByteUtilsTest {
+	@Test
+	public void getUIntSize2ShouldWorkFor0() {
+		assertArrayEquals("Long(2) : 0 => 0",
+				new byte[]{0, 0},
+				ULongByteUtils.fromLong(0, 2));
+	}
+
+	@Test
+	public void getUIntSize2ShouldWorkFor1() {
+		assertArrayEquals("Long(2) : 1 => 1",
+				new byte[]{1, 0},
+				ULongByteUtils.fromLong(1, 2));
+	}
+
+	@Test
+	public void getUIntSize2ShouldWorkForMaxValue() {
+		assertArrayEquals("Long(2) : 65535 => 65535",
+				new byte[]{-1, -1},
+				ULongByteUtils.fromLong(65535, 2));
+	}
+
+	@Test
+	public void getUIntSize2ShouldWorkFor30() {
+		assertArrayEquals("Long(2) : 30 => 30",
+				new byte[]{30, 0},
+				ULongByteUtils.fromLong(30, 2));
+	}
+}
