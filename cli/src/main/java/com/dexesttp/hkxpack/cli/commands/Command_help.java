6a4801a509562693c6abfc2904577c0f6922f036
DexesTTP <dexes.ttp@gmail.com>
2016-04-09 23:02:48 +0200

    Updated help

--- cli/src/main/java/com/dexesttp/hkxpack/cli/commands/Command_help.java
+++ cli/src/main/java/com/dexesttp/hkxpack/cli/commands/Command_help.java
@@ -1,34 +1,30 @@
 package com.dexesttp.hkxpack.cli.commands;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.PrintStream;
 
-import com.dexesttp.hkxpack.cli.ConsoleView;
 import com.dexesttp.hkxpack.cli.utils.StaticProperties;
 
 /**
  * Displays the help of the Command Line Interface.
  */
 public class Command_help implements Command {
-	private static final Logger LOGGER = Logger.getLogger(ConsoleView.class.getName());
 	@Override
 	/**
 	 * {@inheritDoc}
 	 */
 	// TODO prettify help.
 	public int execute(final String... parameters) {
-		if(LOGGER.isLoggable(Level.INFO)) {
-			LOGGER.info("HKXPack version " + StaticProperties.getVersionNumber() );
-			LOGGER.info("Use : java -jar hkxpack-cli.jar <args>");
-			LOGGER.info("Arguments :");
-			LOGGER.info("\t"+"unpack"	+"\t" +"<filename>" + "\t\t" + "Extracts <filename>.hkx into <filename>.xml");
-			LOGGER.info("\t\t\t" +"-o <outputfile>" + "\t" + "Set the output file");
-			LOGGER.info("\t"+"pack"	+"\t" +"<filename>" + "\t\t" + "Compress <filename>.xml into <filename>.hkx");
-			LOGGER.info("\t\t\t" +"-o <outputfile>" + "\t" + "Set the output file");
-			LOGGER.info("\t"+"help"		+"\t\t\t\t"+ "Show this window");
-			LOGGER.info("");
-			LOGGER.info("Report bugs or findings at github.com/dexesttp/hkxpack");
-		}
+		PrintStream out = System.out;
+		out.println("HKXPack version " + StaticProperties.getVersionNumber() );
+		out.println("Use : java -jar hkxpack-cli.jar <args>");
+		out.println("Arguments :");
+		out.println("\t"+"unpack"	+"\t" +"<filename>" + "\t\t" + "Extracts <filename>.hkx into <filename>.xml");
+		out.println("\t\t\t" +"-o <outputfile>" + "\t" + "Set the output file");
+		out.println("\t"+"pack"	+"\t" +"<filename>" + "\t\t" + "Compress <filename>.xml into <filename>.hkx");
+		out.println("\t\t\t" +"-o <outputfile>" + "\t" + "Set the output file");
+		out.println("\t"+"help"		+"\t\t\t\t"+ "Show this window");
+		out.println();
+		out.println("Report bugs or findings at github.com/dexesttp/hkxpack");
 		return 0;
 	}
 }
