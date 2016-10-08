4a792dbe14e7d4ff170226b363e98e5fe1e8567f
DexesTTP <dexes.ttp@gmail.com>
2016-10-08 16:38:47 +0200

    Allow using a Document when reading XML instead of a file only. Moved file to XML resolution from read method to constructor in TagXMLReader.

--- core/src/main/java/com/dexesttp/hkxpack/tagreader/TagXMLReader.java
+++ core/src/main/java/com/dexesttp/hkxpack/tagreader/TagXMLReader.java
@@ -22,33 +22,44 @@ import com.dexesttp.hkxpack.tagreader.exceptions.InvalidTagXMLException;
  * Use {@link TagXMLReader#read()} to process the file.
  */
 public class TagXMLReader {
-	private final transient File tagFile;
 	private final transient HKXDescriptorFactory descriptorFactory;
+	private final transient Document document;
+	private final transient TagXMLFileHandler handler;
 
 	/**
 	 * Creates a TagXML reader.
 	 * @param file the {@link File} to read from.
 	 * @param descriptorFactory the {@link HKXDescriptorFactory} to use while parsing the file.
+	 * @throws IOException if there was an error accessing the {@link File}.
+	 * @throws SAXException if there was an error parsing the XML file.
+	 * @throws ParserConfigurationException should never happen, please report this error if it happens : <br >
+	 * <a href="https://github.com/Dexesttp/hkxpack/issues">The HKXPack's issue tracker</a>
 	 */
-	public TagXMLReader(final File file, final HKXDescriptorFactory descriptorFactory) {
-		this.tagFile = file;
+	public TagXMLReader(final File file, final HKXDescriptorFactory descriptorFactory)
+			throws ParserConfigurationException, SAXException, IOException {
+		this.handler = new TagXMLFileHandler(file);
+		this.document = handler.getDocument();
+		this.descriptorFactory = descriptorFactory;
+	}
+
+	/**
+	 * Creates a TagXML reader from an existing {@link Document}
+	 * @param file the {@link Document} to read from.
+	 * @param descriptorFactory the {@link HKXDescriptorFactory} to use while parsing the file.
+	 */
+	public TagXMLReader(final Document document, final HKXDescriptorFactory descriptorFactory) {
+		this.handler = new TagXMLFileHandler(null);
+		this.document = document;
 		this.descriptorFactory = descriptorFactory;
 	}
 
 	/**
 	 * Read the data from the file.
 	 * @return the read {@link HKXFile}
-	 * @throws IOException if there was an error accessing the {@link File}.
-	 * @throws SAXException if there was an error parsing the XML file.
+	 * @throws IOException if there was an error accessing a descriptor.
 	 * @throws InvalidTagXMLException if there was an error retrieving relevant TagXML elements.
-	 * @throws ParserConfigurationException should never happen, please report this error if it happens : <br >
-	 * <a href="https://github.com/Dexesttp/hkxpack/issues">The HKXPack's issue tracker</a>
 	 */
-	public HKXFile read() throws ParserConfigurationException, SAXException, IOException, InvalidTagXMLException {
-		// Parse the XML file into a DOM.
-		TagXMLFileHandler handler = new TagXMLFileHandler(tagFile);
-		Document document = handler.getDocument();
-		
+	public HKXFile read() throws IOException, InvalidTagXMLException {
 		// Retrieve the section node.
 		Node section = handler.getSectionNode(document, "__data__");
 		if(section == null) {
